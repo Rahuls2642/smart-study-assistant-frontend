65f71246dfb6984092e77a4c343d2ed11b1180b2
Rahul singh <135409027+Rahuls2642@users.noreply.github.com>
2026-04-13 13:24:36 +0530

    Change API base URL to production endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,24 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://smart-study-assistant-backend-bgcg.onrender.com/api",
+});
+
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
+});
+
+export default API;/api",
 });
 
 
